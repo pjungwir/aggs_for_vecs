cf8c8ea15b2e58a8fab4c79f0b7faebddf208b54
Matt Magoffin <matt@solarnetwork.net>
2021-12-16 21:52:12 +1300

    Add more tests for int2/int4/int8/float4/float8 vec_agg_* aggregates.

--- sql/vec_agg_stat.sql
+++ sql/vec_agg_stat.sql
@@ -1,3 +1,43 @@
+-- int2 stats
+SELECT vec_agg_count(smallints) AS counts
+	, vec_agg_sum(smallints) AS sums
+	, vec_agg_mean(smallints) AS means
+	, vec_agg_min(smallints) AS mins
+	, vec_agg_max(smallints) AS maxes
+FROM measurements;
+
+-- int4 stats
+SELECT vec_agg_count(ints) AS counts
+	, vec_agg_sum(ints) AS sums
+	, vec_agg_mean(ints) AS means
+	, vec_agg_min(ints) AS mins
+	, vec_agg_max(ints) AS maxes
+FROM measurements;
+
+-- int8 stats
+SELECT vec_agg_count(bigints) AS counts
+	, vec_agg_sum(bigints) AS sums
+	, vec_agg_mean(bigints) AS means
+	, vec_agg_min(bigints) AS mins
+	, vec_agg_max(bigints) AS maxes
+FROM measurements;
+
+-- float4 stats
+SELECT vec_agg_count(reals) AS counts
+	, vec_agg_sum(reals) AS sums
+	, vec_agg_mean(reals) AS means
+	, vec_agg_min(reals) AS mins
+	, vec_agg_max(reals) AS maxes
+FROM measurements;
+
+-- float8 stats
+SELECT vec_agg_count(floats) AS counts
+	, vec_agg_sum(floats) AS sums
+	, vec_agg_mean(floats) AS means
+	, vec_agg_min(floats) AS mins
+	, vec_agg_max(floats) AS maxes
+FROM measurements;
+
 -- numeric stats
 SELECT vec_agg_count(nums) AS counts
 	, vec_agg_sum(nums) AS sums
